07e112820b220b61ed49aec40b7fedeeec3eb61c
deepakdas777 <deepakdasvkd007@gmail.com>
2017-01-16 13:23:57 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,10 +11,13 @@ def result(request):
 	return render(request, 'result.html', {'total_score': total_score})
 
 def get_score(request, questions):
+	''' this function evaluate the answers submitted by the user,
+	    if it is correct the total score is incrimented by one and
+	    return the total score of the user '''
 	total_score = 0
 	for question in questions:
-		answer = question.answer
-		user_answer = request.POST.get(str(question.id))
-		if(user_answer == answer):
+		answer = question.answer#fetching the correct answer
+		user_answer = request.POST.get(str(question.id))#fetching the answer submitted by the user
+		if(user_answer == answer):#evaluating the answer
 			total_score += 1
 	return total_score
